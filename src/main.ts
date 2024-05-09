e52c2a306bb5d409f1dc7be0dfb1fb0089976e40
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-05-09 12:00:59 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 // import '/src/basic/2'
 // import '/src/basic/3'
 // import '/src/basic/4'
-import '/src/basic/5'
+// import '/src/basic/5'
 // import '/src/basic/6'
 // import '/src/basic/7'
 // import '/src/generics/1'
